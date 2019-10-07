a04c9673cb6f8428c271f8aa827ae5e767196cc6
Alexandre <contact@steptosuccess.fr>
2019-10-07 22:12:27 +0200

    ajout fixtures et api

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\HttpFoundation\Response;
-
+use App\Entity\User;
+use App\Form\UserType;
 use App\Entity\Products;
 use App\Repository\ProductsRepository;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ShopController extends AbstractController
 {
@@ -17,15 +20,52 @@ class ShopController extends AbstractController
         $this->repository = $repository;
         $this->manager = $manager;
     }
+
     /**
      * @Route("/", name="home")
      * @return Response
      */
     public function index() : Response
     {
-        $products = $this->repository->findAll();
+                $products = $this->repository->findAll();
         return $this->render('shop/index.html.twig', [
             'products' => $products
+        ]);
+    }
+
+
+    /**
+     * @Route("../user/login", name="login")
+     * @return Response
+     */
+    public function login() : Response
+    {
+        $products = $this->repository->findAll();
+        return $this->render('user/login.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("../user/signup", name="signup")
+     * @return Response
+     */
+    public function signup(User $user, Request $request) : Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->manager->persist($user);
+            $this->manager->flush();
+            $this->addFlash("success", "Votre compte est créé avec succès");
+            return $this->redirectToRoute('login');
+        }
+
+        $products = $this->repository->findAll();
+        return $this->render('user/signup.html.twig', [
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -10,63 +10,62 @@ use Cocur\Slugify\Slugify;
  */
 class Products
 {
-    const TYPE =[
-        0=> 'Monstre normal',
-        1=> 'Monstre à effet',
-        2=> 'Magie',
-        3=> 'Piège',
-        4=> 'Token'
-    ];
-    const RARITY =[
-        0=> 'Commune',
-        1=> 'Rare'
-    ];
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
-
+    
     /**
      * @ORM\Column(type="string", length=100)
      */
     private $name;
-
+    
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="text", nullable=true)
      */
-    private $type;
+    private $desc;
 
     /**
      * @ORM\Column(type="string", length=100)
      */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
     private $img;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $imgSmall;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $race;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $archetype;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $set_name;
+
+        /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $set_code;
 
     /**
      * @ORM\Column(type="integer")
      */
-    private $rarity;
-
-    /**
-     * @ORM\Column(type="integer", options={"default": 0})
-     */
-    private $abimee;
-
-    /**
-     * @ORM\Column(type="integer", options={"default": 0})
-     */
-    private $occasion;
-
-    /**
-     * @ORM\Column(type="integer", options={"default": 0})
-     */
-    private $correct;
-
-    /**
-     * @ORM\Column(type="integer", options={"default": 0})
-     */
-    private $neuve;
+    private $set_rarity;
 
     /**
      * @ORM\Column(type="float", options={"default": 0})
@@ -89,42 +88,18 @@ class Products
     private $def;
 
     /**
-     * @ORM\Column(type="text", nullable=true)
+     * @ORM\Column(type="integer", length=2)
      */
-    private $text;
+    private $level;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $attribute;
 
     public function getSlug(): string
     {
      return (new Slugify())->slugify($this->name);
-    }
-
-    public function getType(): ?int
-    {
-        return $this->type;
-    }
-
-    public function setType(int $type): self
-    {
-        $this->type = $type;
-
-        return $this;
     }
 
     public function getTypeType(): string
@@ -132,147 +107,356 @@ class Products
         return self::TYPE[$this->type];
     }
 
-    public function getImg(): ?string
-    {
-        return $this->img;
-    }
-
-    public function setImg(string $img): self
-    {
-        $this->img = $img;
-
-        return $this;
-    }
-
-    public function getRarity(): ?int
-    {
-        return $this->rarity;
-    }
-
-    public function setRarity(int $rarity): self
-    {
-        $this->rarity = $rarity;
-
-        return $this;
-    }
-
     public function getRarityType(): string
     {
         return self::RARITY[$this->rarity];
-    }
-
-    public function getAbimee(): ?int
-    {
-        return $this->abimee;
-    }
-
-    public function setAbimee(int $abimee): self
-    {
-        $this->abimee = $abimee;
-
-        return $this;
-    }
-
-    public function getOccasion(): ?int
-    {
-        return $this->occasion;
-    }
-
-    public function setOccasion(int $occasion): self
-    {
-        $this->occasion = $occasion;
-
-        return $this;
-    }
-
-    public function getCorrect(): ?int
-    {
-        return $this->correct;
-    }
-
-    public function setCorrect(int $correct): self
-    {
-        $this->correct = $correct;
-
-        return $this;
-    }
-
-    public function getNeuve(): ?int
-    {
-        return $this->neuve;
-    }
-
-    public function setNeuve(int $neuve): self
-    {
-        $this->neuve = $neuve;
-
-        return $this;
-    }
-
-    public function getCost(): ?float
-    {
-        return $this->cost;
-    }
-
-    public function setCost(float $cost): self
-    {
-        $this->cost = $cost;
-
-        return $this;
     }
 
     public function getFormattedCost(): string {
         return number_format($this->cost, 2, ',', ' ');
     }
 
-    public function getPrice(): ?float
+    public function getFormattedPrice(): string {
+        return number_format($this->price, 2, ',', ' ');
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     *
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     *
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+
+    /**
+     * @param mixed $desc
+     *
+     * @return self
+     */
+    public function setDesc($desc)
+    {
+        $this->desc = $desc;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     *
+     * @return self
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * @param mixed $img
+     *
+     * @return self
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImgSmall()
+    {
+        return $this->imgSmall;
+    }
+
+    /**
+     * @param mixed $imgSmall
+     *
+     * @return self
+     */
+    public function setImgSmall($imgSmall)
+    {
+        $this->imgSmall = $imgSmall;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRace()
+    {
+        return $this->race;
+    }
+
+    /**
+     * @param mixed $race
+     *
+     * @return self
+     */
+    public function setRace($race)
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArchetype()
+    {
+        return $this->archetype;
+    }
+
+    /**
+     * @param mixed $archetype
+     *
+     * @return self
+     */
+    public function setArchetype($archetype)
+    {
+        $this->archetype = $archetype;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSetName()
+    {
+        return $this->set_name;
+    }
+
+    /**
+     * @param mixed $set_name
+     *
+     * @return self
+     */
+    public function setSetName($set_name)
+    {
+        $this->set_name = $set_name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSetCode()
+    {
+        return $this->set_code;
+    }
+
+    /**
+     * @param mixed $set_code
+     *
+     * @return self
+     */
+    public function setSetCode($set_code)
+    {
+        $this->set_code = $set_code;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSetRarity()
+    {
+        return $this->set_rarity;
+    }
+
+    /**
+     * @param mixed $set_rarity
+     *
+     * @return self
+     */
+    public function setSetRarity($set_rarity)
+    {
+        $this->set_rarity = $set_rarity;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCost()
+    {
+        return $this->cost;
+    }
+
+    /**
+     * @param mixed $cost
+     *
+     * @return self
+     */
+    public function setCost($cost)
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
     {
         return $this->price;
     }
 
-    public function setPrice(float $price): self
+    /**
+     * @param mixed $price
+     *
+     * @return self
+     */
+    public function setPrice($price)
     {
         $this->price = $price;
 
         return $this;
     }
 
-    public function getFormattedPrice(): string {
-        return number_format($this->price, 2, ',', ' ');
-    }
-
-    public function getAtk(): ?string
+    /**
+     * @return mixed
+     */
+    public function getAtk()
     {
         return $this->atk;
     }
 
-    public function setAtk(string $atk): self
+    /**
+     * @param mixed $atk
+     *
+     * @return self
+     */
+    public function setAtk($atk)
     {
         $this->atk = $atk;
 
         return $this;
     }
 
-    public function getDef(): ?string
+    /**
+     * @return mixed
+     */
+    public function getDef()
     {
         return $this->def;
     }
 
-    public function setDef(string $def): self
+    /**
+     * @param mixed $def
+     *
+     * @return self
+     */
+    public function setDef($def)
     {
         $this->def = $def;
 
         return $this;
     }
 
-    public function getText(): ?string
+    /**
+     * @return mixed
+     */
+    public function getLevel()
     {
-        return $this->text;
+        return $this->level;
     }
 
-    public function setText(?string $text): self
+    /**
+     * @param mixed $level
+     *
+     * @return self
+     */
+    public function setLevel($level)
     {
-        $this->text = $text;
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAttribute()
+    {
+        return $this->attribute;
+    }
+
+    /**
+     * @param mixed $attribute
+     *
+     * @return self
+     */
+    public function setAttribute($attribute)
+    {
+        $this->attribute = $attribute;
 
         return $this;
     }
