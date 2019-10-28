620ca71ba5af52390934168825c0b4045c96a968
Alexandre <gaillotalexandre@yahoo.fr>
2019-10-28 22:53:15 +0100

    tests form user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,23 +6,30 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $user = new User();
+        $user->setCreatedAt(new \Datetime())
+            ->setCodeValidation('creer code et balancer ici');
+
         $builder
             ->add('nom')
             ->add('prenom')
             ->add('adresse')
             ->add('code_postal')
             ->add('ville')
-            ->add('email')
+            ->add('email', EmailType::Class)
             ->add('telephone')
-            ->add('password')
-            ->add('code_validation')
-            ->add('valide')
-            ->add('created_at')
+            ->add('password', PasswordType::Class)
+            ->add('password_verify', PasswordType::Class, ['mapped' => false])
+            // ->add('code_validation')
+            // ->add('valide')
+            // ->add('created_at')
         ;
     }
 
