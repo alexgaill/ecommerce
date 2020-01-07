bc3959bcfb10b68ea75ee92997c95e3c2eb00139
Alexandre <gaillotalexandre@yahoo.fr>
2020-01-07 23:45:14 +0100

    arrivage v6

--- assets/js/arrivage.js
+++ assets/js/arrivage.js
@@ -2,17 +2,29 @@ let i = 1;
 
 function getCards(datas, elem) {
     for ( let data of datas) {
-        elem.append('<option value="'+ data.setCode +'">' + data.name +' - '+ data.setCode+ '</option>')
+        elem.append('<option value="'+ data.setCode +'">' + data.name +' - '+ data.setCode+ '</option>');
     }
 }
 
 function totalPrice(cost, newCard, correct, occasion, abimee){
-    let totalNew = cost * newCard
-    let totalCorrect = cost * correct * 0.9
-    let totalOccasion = cost * occasion * 0.8
-    let totalAbimee = cost * abimee * 0.6
+    let totalNew = cost * newCard;
+    let totalCorrect = cost * correct * 0.9;
+    let totalOccasion = cost * occasion * 0.8;
+    let totalAbimee = cost * abimee * 0.6;
 
-    return totalNew + totalCorrect + totalOccasion + totalAbimee
+    return totalNew + totalCorrect + totalOccasion + totalAbimee;
+}
+
+function createLine (key){
+    $("tbody").append("<tr class='ajout "+ key +"'></tr>");
+        $("."+ key).append('<td class="cardName"><input list="cardName'+ key +'" name="cardName"><datalist id="cardName'+ key +'"></datalist></td>');
+        $("."+ key).append('<td><input type="text" name="typeEntry" class="typeEntry col-lg-auto"></td>');
+        $("."+ key).append('<td><input type="text" name="new" class="new col-lg-auto"></td>');
+        $("."+ key).append('<td><input type="text" name="correct" class="correct col-lg-auto"></td>');
+        $("."+ key).append('<td><input type="text" name="occasion" class="occasion col-lg-auto"></td>');
+        $("."+ key).append('<td><input type="text" name="abimee" class="abimee col-lg-auto"></td>');
+        $("."+ key).append('<td class="prixAchat col-lg-auto"></td>');
+        $("."+ key).append('<td class="total"></td>');
 }
 
 $("#sellerId").change(function(){
@@ -22,38 +34,37 @@ $("#sellerId").change(function(){
         dataType: 'json'
     })
     .done(function (data) {
-        $("#nomClient").val(data.nom)
-        $("#prenomClient").val(data.prenom)
-        $("#telephone").val(data.telephone)
-        $("#mail").val(data.email)
-        $("#adresse").val(data.adresse)
-        $("#codePostal").val(data.code_postal)
-        $("#ville").val(data.ville)
+        $("#nomClient").val(data.nom);
+        $("#prenomClient").val(data.prenom);
+        $("#telephone").val(data.telephone);
+        $("#mail").val(data.email);
+        $("#adresse").val(data.adresse);
+        $("#codePostal").val(data.code_postal);
+        $("#ville").val(data.ville);
     })
 })
 
-$("#addMore").click(function(){
-    $("tbody").append("<tr class='ajout "+ i +"'></tr>")
-    $("."+ i).append('<td class="cardName"><input list="cardName'+ i +'" name="cardName"><datalist id="cardName'+ i +'"></datalist></td>')
-    $("."+ i).append('<td><input type="text" name="typeEntry" class="typeEntry col-lg-auto"></td>')
-    $("."+ i).append('<td><input type="text" name="new" class="new col-lg-auto"></td>')
-    $("."+ i).append('<td><input type="text" name="correct" class="correct col-lg-auto"></td>')
-    $("."+ i).append('<td><input type="text" name="occasion" class="occasion col-lg-auto"></td>')
-    $("."+ i).append('<td><input type="text" name="abimee" class="abimee col-lg-auto"></td>')
-    $("."+ i).append('<td><input type="text" name="prixAchat" class="prixAchat col-lg-auto"></td>')
-    $("."+ i).append('<td class="total"></td>')
-
-    let selectCardNameDatalist = $("."+ i + " .cardName datalist")
-    let selectCardNameInput = $("."+ i + " .cardName input")
-    let selectCostPrice = $("." + i + " .prixAchat")
-
-    $.ajax({
+$.ajax({
         url:"./getCards",
         method:"GET",
         dataType:"json"
-    })
-    .done(function(datas){
-        getCards(datas, selectCardNameDatalist)
+})
+.done(function(datas){
+    $("#addMore").click(function(){
+        
+        createLine(i);
+
+        let selectCardNameDatalist = $("."+ i + " .cardName datalist");
+        let selectCardNameInput = $("."+ i + " .cardName input");
+        let selectCostPrice = $("." + i + " .prixAchat");
+        let total = $("." + i + " .total");
+        let newQty = $("." + i + " .new");
+        let correctQty = $("." + i + " .correct");
+        let occasionQty = $("." + i + " .occasion");
+        let abimeeQty = $("." + i + " .abimee");
+
+        getCards(datas, selectCardNameDatalist);
+
         selectCardNameInput.change(function(){
             $.ajax({
                 url:"./getCost/" + $(this).val(),
@@ -61,13 +72,27 @@ $("#addMore").click(function(){
                 dataType:"json"
             })
             .done(function(datas){
-                selectCostPrice.val(datas.cost)
-                $(this).parent().children('.new').change(function(){
-                    console.log('ok')
+                selectCostPrice.textContent = datas.cost;
+
+                newQty.change(function(){ 
+                    let totalPrice = totalPrice(datas.cost, newQty.val(), correctQty.val(), occasionQty.val(), abimeeQty.val());
+                    total.textContent = totalPrice.toFixed(2);
+                })
+                correctQty.change(function(){ 
+                    let totalPrice = totalPrice(datas.cost, newQty.val(), correctQty.val(), occasionQty.val(), abimeeQty.val());
+                    total.textContent = totalPrice.toFixed(2);       
+                })
+                occasionQty.change(function(){      
+                    let totalPrice = totalPrice(datas.cost, newQty.val(), correctQty.val(), occasionQty.val(), abimeeQty.val());
+                    total.textContent = totalPrice.toFixed(2);  
+                })
+                abimeeQty.change(function(){      
+                    let totalPrice = totalPrice(datas.cost, newQty.val(), correctQty.val(), occasionQty.val(), abimeeQty.val());
+                    total.textContent = totalPrice.toFixed(2);  
                 })
             })
         })
+        return i ++
     })
 
-    return i ++
 })
